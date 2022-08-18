cedf1d91f0406bf67ba6dfe590a10fad0ffaf67b
Austin Borck <austinborck98@gmail.com>
2022-08-18 11:58:58 -0600

    Create BootMVC Project and link depend

---
+++ PizzaPalette/src/main/java/com/skilldistillery/pizzapalette/PizzaPaletteApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.pizzapalette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaPaletteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaPaletteApplication.class, args);
+	}
+
+}
