a8767d7808255ae867609df56ae611a797d75e9f
Austin Borck <austinborck98@gmail.com>
2022-08-19 15:27:39 -0600

    Added login/logout function

--- PizzaPalette/src/main/java/com/skilldistillery/pizzapalette/controllers/UserController.java
+++ PizzaPalette/src/main/java/com/skilldistillery/pizzapalette/controllers/UserController.java
@@ -1,11 +1,15 @@
 package com.skilldistillery.pizzapalette.controllers;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.skilldistillery.pizzapalette.data.UserDAO;
+import com.skilldistillery.pizzapalette.entities.User;
 
 @Controller
 public class UserController {
@@ -13,13 +17,32 @@ public class UserController {
 	@Autowired
 	private UserDAO userDao;
 	
-	@RequestMapping(path = {"/", "home.do"})
+	@RequestMapping(path = {"/", "index.do"})
 	public String home(Model model) {
-		model.addAttribute("SMOKETEST", userDao.findById(1));  //delete later
-		return "home";
+		return "index";
 	}
 	
+	@RequestMapping(path = "loginButton.do")
+	public String loginPage(Model model) {
+		return "loginPage";
+	}
 	
+	@RequestMapping(path = "login.do", method=RequestMethod.POST)
+	public String logInUser(HttpSession session, Model model, String username, String password) {
+		User user = userDao.login(username, password);
+				if (user == null) {
+					return "loginPage";
+				} else {
+					session.setAttribute("loggedInUser", user);
+					return "accountPage";
+				}
+		//ADD IF STATEMENTS FOR THE RETURNED USER FROM THE DAOIMPL METHOD
+	}
 	
+	@RequestMapping("logout.do")
+	public String logout(HttpSession session) {
+		session.removeAttribute("loggedInUser");
+		return "index";
+	}
 	
 }
