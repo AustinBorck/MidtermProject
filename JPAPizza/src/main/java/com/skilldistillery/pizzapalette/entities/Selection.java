82adfc6115b6d62f15ed901087b169f3955684e5
Austin Borck <austinborck98@gmail.com>
2022-08-19 10:37:17 -0600

    Half entities done

--- JPAPizza/src/main/java/com/skilldistillery/pizzapalette/entities/Selection.java
+++ JPAPizza/src/main/java/com/skilldistillery/pizzapalette/entities/Selection.java
@@ -1,8 +1,73 @@
 package com.skilldistillery.pizzapalette.entities;
 
+import java.util.Objects;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Selection {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String name;
+
+	private String description;
+
+	// NO ARGS CONSTRUCTOR
+	public Selection() {
+	}
+
+	// GETTERS SETTERS
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Selection other = (Selection) obj;
+		return id == other.id;
+	}
+
+	// TO STRING
+	@Override
+	public String toString() {
+		return "Selection [id=" + id + ", name=" + name + ", description=" + description + "]";
+	}
 
 }
