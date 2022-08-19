fe8c31033181c791cb012b37285f2978fc0dbb1a
tblandin <tiarra.blandin@outlook.com>
2022-08-19 15:16:29 -0600

    login DAO

--- PizzaPalette/src/main/java/com/skilldistillery/pizzapalette/data/UserDAO.java
+++ PizzaPalette/src/main/java/com/skilldistillery/pizzapalette/data/UserDAO.java
@@ -1,10 +1,49 @@
 package com.skilldistillery.pizzapalette.data;
 
+import java.util.List;
+
+import com.skilldistillery.pizzapalette.entities.Address;
+import com.skilldistillery.pizzapalette.entities.Attribute;
+import com.skilldistillery.pizzapalette.entities.PizzaJoint;
+import com.skilldistillery.pizzapalette.entities.Review;
 import com.skilldistillery.pizzapalette.entities.User;
 
 public interface UserDAO {
 	
+	User findUsername(String username);
 	
-	User findById(int userId);
+	List<PizzaJoint> findName(String name);
+	
+	List<Address> findCity(String city);
+	
+	List<Address> findState(String state);
+	
+	List<Address> findZip(String zip);
+	
+	List<Attribute> findAttribute(String name);
+	
+	//////////////////////////////////////////////////////////
+	
+	public User login(String username, String password);
+	
+	public User addUser(int id);
+	
+	public User editUser(int id);
+
+	public boolean deactivateUser(int id);
+	
+	public PizzaJoint addPizzaJoint();
+	
+	public PizzaJoint editPizzaJoint();
+	
+	public PizzaJoint deactivatePizzaJoint();
+	
+	public Review addRating();
+	
+	public Review addComments();
+	
+	public Review deactivateRating();
+	
+	public Review deactivateComments();
 	
 }
--- PizzaPalette/src/main/java/com/skilldistillery/pizzapalette/data/UserDaoImpl.java
+++ PizzaPalette/src/main/java/com/skilldistillery/pizzapalette/data/UserDaoImpl.java
@@ -1,11 +1,19 @@
 package com.skilldistillery.pizzapalette.data;
 
+import java.awt.print.Book;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
 
+import com.skilldistillery.pizzapalette.entities.Address;
+import com.skilldistillery.pizzapalette.entities.Attribute;
+import com.skilldistillery.pizzapalette.entities.PizzaJoint;
+import com.skilldistillery.pizzapalette.entities.Review;
 import com.skilldistillery.pizzapalette.entities.User;
 
 @Service
@@ -16,8 +24,108 @@ public class UserDaoImpl implements UserDAO {
 	private EntityManager em;
 
 	@Override
-	public User findById(int userId) {
-		return em.find(User.class, userId);
+	public User findUsername(String username) {
+			return em.find(User.class, username);
+	}
+
+	@Override
+	public List<PizzaJoint> findName(String name) {
+		List<PizzaJoint> pizzaJointList = new ArrayList<>();
+		String jpql = "SELECT p FROM PizzaJoint p WHERE p.name LIKE :name";
+		pizzaJointList = em.createQuery(jpql, PizzaJoint.class)
+					 .setParameter("name", "%" + name + "%")
+					 .getResultList();
+		return pizzaJointList;
+	}
+
+	@Override
+	public List<Address> findCity(String city) {
+		List<Address> pizzaJointList = new ArrayList<>();
+		String jpql = "SELECT p FROM Pizza_joint p WHERE p.city LIKE :city";
+		pizzaJointList = em.createQuery(jpql, Address.class)
+					 .setParameter("city", "%" + city + "%")
+					 .getResultList();
+		return pizzaJointList;
+	}
+
+	@Override
+	public List<Address> findState(String state) {
+		return null;
+	}
+
+	@Override
+	public List<Address> findZip(String zip) {
+		return null;
+	}
+
+	@Override
+	public List<Attribute> findAttribute(String name) {
+		return null;
+	}
+
+	@Override
+	public User login(String username, String password) {
+		String jpql = "SELECT u FROM User u WHERE u.username = :u AND u.password = :p AND u.enabled = true";
+		User user = null;
+		try {
+			user = em.createQuery(jpql, User.class)
+					.setParameter("u", username)
+					.setParameter("p", password)
+					.getSingleResult();
+		} catch (Exception e) {
+			System.out.println("Invalid login");
+		}
+		return user;
+	}
+
+	@Override
+	public User addUser(int id) {
+		return null;
+	}
+
+	@Override
+	public User editUser(int id) {
+		return null;
+	}
+
+	@Override
+	public boolean deactivateUser(int id) {
+		return false;
+	}
+
+	@Override
+	public PizzaJoint addPizzaJoint() {
+		return null;
+	}
+
+	@Override
+	public PizzaJoint editPizzaJoint() {
+		return null;
+	}
+
+	@Override
+	public PizzaJoint deactivatePizzaJoint() {
+		return null;
+	}
+
+	@Override
+	public Review addRating() {
+		return null;
+	}
+
+	@Override
+	public Review addComments() {
+		return null;
+	}
+
+	@Override
+	public Review deactivateRating() {
+		return null;
+	}
+
+	@Override
+	public Review deactivateComments() {
+		return null;
 	}
 	
 }
